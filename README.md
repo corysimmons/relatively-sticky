0d226b4cb1220310476bbda1cbde9e2bca66c3af
CorySimmons <cory@pressedweb.com>
2013-09-30 19:45:32 -0400

    Add initial js

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ relatively-sticky
 =================
 
 A jQuery plugin that will create "fixed" (they're really relative) elements that are relative to things
+
+Right now this is very specific to my (totally friggin' sweet) [Jeet 3 Framework](http://jeetframework.com), but I plan on decoupling this for the people!
---
+++ relatively-sticky.js
@@ -0,0 +1,40 @@
+$(function() {
+    
+    // Jeet's "Relatively Sticky" plugin
+    if($('[jeet-fixed]').attr('jeet-fixed')) {
+        var setPosition = function() {
+            $('[jeet-fixed]').css({
+                position: 'relative',
+                top: $(window).scrollTop() + parseInt($('[jeet-fixed]').attr('jeet-fixed'), 10)
+            });
+        };
+        $(window).scroll(function() {
+            if($(window).scrollTop() >= parseInt($('[jeet-pickup]').attr('jeet-pickup'), 10)) {
+                setPosition();
+                $(window).scroll(function() {
+                    var jS;
+                    if($('[jeet-speed]').attr('jeet-speed')) {
+                        jS = $('[jeet-speed]').attr('jeet-speed');
+                    } else {
+                        jS = '200ms';
+                    }
+                    setInterval(function() {
+                        if($(window).scrollTop() >= parseInt($('[jeet-pickup]').attr('jeet-pickup'), 10)) {
+                            $('[jeet-fixed]').css({
+                                position: 'relative',
+                                top: $(window).scrollTop() + parseInt($('[jeet-fixed]').attr('jeet-fixed'), 10),
+                                transition: 'all '+ jS +' ease'
+                            });
+                        } else {
+                            $('[jeet-fixed]').css({
+                                position: 'relative',
+                                top: 'auto'
+                            });
+                        }
+                    }, 200);
+                });
+            }
+        });
+    }
+
+});
